815fc62536403b8a4f449c6cc9f9944e09545008
Kalyanidosapati <116077159+Kalyanidosapati@users.noreply.github.com>
2026-01-08 19:07:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,14 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-02b8269d5e85954ef"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    
-}
-tags{
+    subnet_id = "subnet-03c9dae1566088a38"
+tags = {
     Name = "newinstance"
+}
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+tags = {
+    Name  = "My bucket"
+  }
 }
